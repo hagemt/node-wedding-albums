ebc8f8bddd7481b53b6d322a33c2abbf25d89d05
Tor Hagemann <tohagema@cisco.com>
2017-09-18 20:04:13 -0700

    components: update to use favorites

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,26 +1,39 @@
+/* eslint-env browser */
+import URL from 'url'
+
 import React from 'react'
 import Types from 'prop-types'
 
-class Image extends React.Component {
+const buildFavoriteImageLink = (url, number) => {
+	const { origin } = new URL(url) // parser
+	return `${origin}/favorites?id=${number}`
+}
 
-	render () {
-		const { number, url } = this.props
-		const padded = String(number).padStart(4, '0')
-		const altText = `Image #${padded} (${url})`
-		const imageLink = `${url}/fullsize/${padded}.jpg`
-		const thumbnailLink = `${url}/thumbnail/${padded}.png`
-		return (
-			<div className="image">
-				<a href={imageLink}>
-					<img alt={altText} src={thumbnailLink} />
-				</a>
-			</div>
-		)
-	}
+const onClickFavorite = (isFavorite, url) => {
+	const method = isFavorite ? 'DELETE' : 'POST'
+	return () => window.fetch(url, { method })
+}
 
+const Image = ({ isFavorite, number, url }) => {
+	const padded = String(number).padStart(4, '0')
+	const altText = `Image #${padded} (${url})`
+	const imageLink = `${url}/fullsize/${padded}.jpg`
+	const thumbnailLink = `${url}/thumbnail/${padded}.png`
+	const favoritesLink = buildFavoriteImageLink(url, number)
+	const onClick = onClickFavorite(!isFavorite, favoritesLink)
+	const text = isFavorite ? '- Favorite' : '+ Favorite'
+	return (
+		<div className="image">
+			<a href={imageLink}>
+				<img alt={altText} src={thumbnailLink} />
+			</a>
+			<button onClick={onClick}>{text}</button>
+		</div>
+	)
 }
 
 Image.propTypes = {
+	isFavorite: Types.boolean,
 	number: Types.number,
 	url: Types.string,
 }
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,15 +1,54 @@
+import { Map as ImmutableMap } from 'immutable'
+
 import React from 'react'
 
 import Album from './Album.js'
 
-const Root = () => {
-	const title = '2017-08-20'
-	const url = `http://localhost:8000/${title}`
-	return (
-		<div className="root">
-			<Album count={1170} offset={0} title={title} url={url} />
-		</div>
-	)
+const album = Object.freeze({
+	title: '2017-08-20',
+	total: 1170,
+})
+
+class Root extends React.Component {
+
+	constructor (...args) {
+		super(...args)
+		this.state = {
+			pages: ImmutableMap.of(album, {
+				offset: 0, // start from first image
+				length: 10, // this is the page size
+			}),
+		}
+	}
+
+	setPage ({ offset, length }) {
+		// TODO: noop if invalid?
+		this.setState(({ pages }) => {
+			if (album.total < offset + length) return {} // noop
+			return { pages: pages.set(album, { offset, length }) }
+		})
+	}
+
+	render () {
+		// eslint-disable-next-line react/prop-types
+		const { length, offset } = this.state.pages.get(album)
+		const url = `http://localhost:8000/albums/${album.title}`
+		const images = Array.from({ length }, (none, index) => (offset + index + 1))
+		const buttons = []
+		if (!(offset - length < 0)) {
+			buttons.shift(<button onClick={() => this.page({ length, offset: offset - length })}>Previous</button>)
+		}
+		if (offset + length < album.total) {
+			buttons.shift(<button onClick={() => this.page({ length, offset: offset + length })}>Next</button>)
+		}
+		return (
+			<div className="root">
+				<Album images={images} title={album.title} url={url} />
+				<div className="album-controls">{buttons}</div>
+			</div>
+		)
+	}
+
 }
 
 export default Root
