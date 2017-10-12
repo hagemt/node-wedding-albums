8bf749c11b09af3bf03e34b0204f54d17949cb84
Tor Hagemann <tohagema@cisco.com>
2017-10-11 22:29:45 -0700

    images: update thumbnail script for arbitrary size

--- images/thumbnail.sh
+++ images/thumbnail.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+function thumbnail {
+	convert -thumbnail "$3x$4" "$1" "$2" && file "$2"
+}
+
+DIRECTORY="$(dirname $0)/2017-08-20"
 for PADDED in $(seq --format="%04g" 1 1170); do
-	echo "converting Image #$PADDED ($(pwd)) to thumbnail"
-	convert -thumbnail 200x200 "fullsize/$PADDED.jpg" "thumbnail/$PADDED.png"
+	echo thumbnail "$DIRECTORY/fullsize/$PADDED.jpg" "$DIRECTORY/thumbnail/$PADDED.png" 640
 done
