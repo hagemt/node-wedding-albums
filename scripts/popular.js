1ccf468c35ea5cb615c251b075ebd9cc7caeb47d
Tor Hagemann <tohagema@cisco.com>
2017-10-16 01:57:15 -0700

    scripts: make `popular` user much more readable

--- scripts/popular.js
+++ scripts/popular.js
@@ -1,65 +1,46 @@
 /* eslint-env es6, node */
-const Process = require('child_process')
-
-const HTTP = require('http')
-const HTTPS = require('https')
+const assert = require('assert')
 const path = require('path')
-const URL = require('url')
 
-const json = url => new Promise((resolve, reject) => {
-	const headers = { 'user-agent': __filename }
-	const options = Object.assign({ headers, method: 'GET' }, URL.parse(url))
-	const request = url.startsWith('https') ? HTTPS.request(options) : HTTP.request(options)
-	request.once('error', reject) // bad end
-	request.once('response', (response) => {
-		if (response.statusCode !== 200) {
-			reject(new Error(response.statusCode))
-			return // don't even bother with body
-		}
-		const buffers = [] // JSON body
-		response.once('error', reject)
-		response.once('end', () => {
-			try {
-				resolve(JSON.parse(Buffer.concat(buffers)))
-			} catch (error) {
-				reject(error)
-			}
-		})
-		response.on('data', (buffer) => {
-			buffers.push(buffer)
-		})
-	})
-	request.end()
-})
+const fetch = require('isomorphic-fetch')
 
-const fetchBoth = async ({ ALBUMS_DOMAIN = 'leah-and-tor.love' }) => {
-	const request = url => json(`http://${ALBUMS_DOMAIN}/${url}`) // => Promise
-	const [laughs, loves] = await Promise.all(['laughs', 'loves'].map(request))
-	const scores = new Map() // Integer -> Sets
-	for (let i = 1, n = 1170; i < n; i += 1) {
-		// any score Function can be substituted here:
-		const score = laughs.images[i] + loves.images[i]
-		const set = scores.get(score) || new Set()
-		scores.set(score, set.add(i)) // by score
+const fetchJSON = async (all, options) => {
+	const responses = await Promise.all(all.map(one => fetch(one, options)))
+	for (const response of responses) assert.equal(response.status, 200, 'status')
+	return Promise.all(responses.map(response => response.json())) // awaitable
+}
+
+const ALBUM_DOMAIN = process.env.ALBUM_DOMAIN || 'leah-and-tor.love'
+const ALBUM_PATH = path.resolve(__dirname, '..', 'images', '2017-08-20')
+const SUM_TOTAL = ({ id, laughs, loves }) => (laughs.images[id] + loves.images[id])
+
+const findPopular = async (score = SUM_TOTAL, ...args) => {
+	const [laughs, loves] = await fetchJSON([
+		`http://${ALBUM_DOMAIN}/laughs?id=${args.join()}`,
+		`http://${ALBUM_DOMAIN}/loves?id=${args.join()}`,
+	])
+	const scores = new Map() // Number -> Sets
+	for (let id = 1; id <= 1170; id += 1) {
+		const number = score({ id, laughs, loves })
+		const set = scores.get(number) || new Set()
+		scores.set(number, set.add(id))
 	}
 	// extract the key-maximal level-set of values, in natural order; i.e. "best" pictures [1, 2, 3]
-	const max = Array.from(scores, ([score]) => score).reduce((best, next) => Math.max(best, next), 0)
-	if (!(max > 0)) throw new Error('max score was not positive; everyone hates everything')
-	const all = Array.from(scores.get(max)).sort((left, right) => (right - left))
-	return Object.freeze(Object.assign(all, { score: max }))
+	const max = Array.from(scores, ([number]) => number).reduce((best, next) => Math.max(best, next), 0)
+	const all = Array.from(scores.get(max)).sort((left, right) => (left - right)) // ascending numbers
+	const resolve = id => path.resolve(ALBUM_PATH, 'fullsize', `${String(id).padStart(4, '0')}.jpg`)
+	return Object.freeze(Object.assign(all.map(one => resolve(one)), { max }))
 }
 
 if (!module.parent) {
-	fetchBoth(process.env)
-		.then((result) => {
-			const album = path.resolve(__dirname, '..', 'images', '2017-08-20', 'fullsize') // path to images
-			const paths = result.map(number => path.resolve(album, `${String(number).padStart(4, '0')}.jpg`))
-			console.log(`eog ${paths.join(' ')} # max: ${result.score}`) // eslint-disable-line no-console
-			Process.spawnSync('/usr/bin/eog', paths, { detached: true }) // open w/ "Eye of Gnome"
+	findPopular()
+		.then((paths) => {
+			/* Image Viewer (Linux): */// eslint-disable-next-line no-console
+			console.log(['eog', ...paths, `# max: ${paths.max}`].join(' \\\n'))
 		})
-		.catch((reason) => {
+		.catch((fatal) => {
 			// eslint-disable-next-line no-console
-			console.error(reason)
+			console.error(fatal)
 			process.exit(1)
 		})
 }
